199def54d86713811b96e255ad4dabc64a9fa9db
Brent <bgardner@squarelabs.net>
2017-12-03 20:15:06 +0100

    Create test database on which everything fun happens

--- src/test/kotlin/TestPostGreSQL.kt
+++ src/test/kotlin/TestPostGreSQL.kt
@@ -1,18 +1,34 @@
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNotEquals
+import org.junit.BeforeClass
 import org.junit.Test
 import java.sql.DriverManager
 
 class TestPostGreSQL {
-    @Test
-    fun sanity() {
-        assertNotEquals("Sanity is in the world", true, false);
+    companion object {
+        @BeforeClass
+        @JvmStatic
+        fun createDatabase() {
+            val conString = "jdbc:postgresql://localhost:5432/postgres";
+            val username = "postgres";
+            val password = "postgres";
+
+            DriverManager.getConnection(conString, username, password).use {
+                it.prepareStatement("drop database if exists test;").use {
+                    it.execute()
+                }
+
+                it.prepareStatement("create database test;").use {
+                    it.execute()
+                }
+            }
+
+        }
     }
 
     @Test
     fun canExecuteQuery() {
         val expected = true;
-        val conString = "jdbc:postgresql://localhost:5432/postgres";
+        val conString = "jdbc:postgresql://localhost:5432/test";
         val username = "postgres";
         val password = "postgres";
 
@@ -28,34 +44,7 @@ class TestPostGreSQL {
         }
     }
 
-    @Test
-    fun canCreateDatabase() {
-        val conString = "jdbc:postgresql://localhost:5432/postgres";
-        val username = "postgres";
-        val password = "postgres";
 
-        DriverManager.getConnection(conString, username, password).use {
-            it.prepareStatement("drop database if exists test;").use {
-                it.execute()
-            }
-
-            it.prepareStatement("create database test;").use {
-                val result = it.execute()
-//                assertEquals("Can create database", true, result);
-            }
-        }
-
-        DriverManager.getConnection("jdbc:postgresql://localhost:5432/test", username, password).use {
-            it.prepareStatement("select true as works;").use {
-                it.executeQuery().use {
-                    val hasRecord = it.next();
-                    assertEquals("Should select 1 record", true, hasRecord);
-                    val actual = it.getBoolean(1);
-                    assertEquals("Should return true", true, actual);
-                }
-            }
-        }
-    }
 }
 
 
