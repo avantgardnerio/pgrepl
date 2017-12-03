91d6fde9d5ed8b491cc4501552f74e5741fde5c3
Brent <bgardner@squarelabs.net>
2017-12-03 21:54:59 +0100

    Dubious about tests in Travis

--- src/test/kotlin/net/squarelabs/pgrepl/DbHelperTest.kt
+++ src/test/kotlin/net/squarelabs/pgrepl/DbHelperTest.kt
@@ -1,6 +1,7 @@
 package net.squarelabs.pgrepl
 
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
 import org.junit.Test
 
 class DbHelperTest {
@@ -9,10 +10,14 @@ class DbHelperTest {
 
     @Test
     fun shouldCrud() {
-        val db = DbHelper("jdbc:postgresql://localhost:5432/postgres?user=postgres&password=postgres");
-        if(db.list().contains(NAME)) db.drop(NAME);
-        assertEquals("should not have test db after drop", false, db.list().contains(NAME));
-        db.create(NAME)
-        assertEquals("should not test db after create", true, db.list().contains(NAME));
+        try {
+            val db = DbHelper("jdbc:postgresql://localhost:5432/postgres?user=postgres&password=postgres");
+            if(db.list().contains(NAME)) db.drop(NAME);
+            assertEquals("should not have test db after drop", false, db.list().contains(NAME));
+            db.create(NAME)
+            assertEquals("should not test db after create", true, db.list().contains(NAME));
+        } catch (ex: Exception) {
+            assertNull(ex);
+        }
     }
 }
