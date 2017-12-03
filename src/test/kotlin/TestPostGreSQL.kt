5252501972917ce10144c3770dec40a89c50c805
Brent <bgardner@squarelabs.net>
2017-12-03 20:07:35 +0100

    Verify we can create a DB

--- src/test/kotlin/TestPostGreSQL.kt
+++ src/test/kotlin/TestPostGreSQL.kt
@@ -20,9 +20,38 @@ class TestPostGreSQL {
             it.prepareStatement("select true as works;").use {
                 it.executeQuery().use {
                     val hasRecord = it.next();
-                    assertEquals("Should select 1 record", hasRecord, true);
+                    assertEquals("Should select 1 record", true, hasRecord);
                     val actual = it.getBoolean(1);
                     assertEquals("Should return true", expected, actual);
+                }
+            }
+        }
+    }
+
+    @Test
+    fun canCreateDatabase() {
+        val conString = "jdbc:postgresql://localhost:5432/postgres";
+        val username = "postgres";
+        val password = "postgres";
+
+        DriverManager.getConnection(conString, username, password).use {
+            it.prepareStatement("drop database if exists test;").use {
+                it.execute()
+            }
+
+            it.prepareStatement("create database test;").use {
+                val result = it.execute()
+//                assertEquals("Can create database", true, result);
+            }
+        }
+
+        DriverManager.getConnection("jdbc:postgresql://localhost:5432/test", username, password).use {
+            it.prepareStatement("select true as works;").use {
+                it.executeQuery().use {
+                    val hasRecord = it.next();
+                    assertEquals("Should select 1 record", true, hasRecord);
+                    val actual = it.getBoolean(1);
+                    assertEquals("Should return true", true, actual);
                 }
             }
         }
