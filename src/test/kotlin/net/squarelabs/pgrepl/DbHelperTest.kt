78892aefbcc0bba8466d03a30de29096ecd167a1
Brent <bgardner@squarelabs.net>
2017-12-04 03:27:32 +0100

    Fuck gradle, full on

--- src/test/kotlin/net/squarelabs/pgrepl/DbHelperTest.kt
+++ src/test/kotlin/net/squarelabs/pgrepl/DbHelperTest.kt
@@ -10,16 +10,13 @@ class DbHelperTest {
 
     @Test
     fun shouldCrud() {
-        try {
-            val conString = "jdbc:postgresql://localhost:5432/postgres?user=postgres&password=postgres"
-            DbHelper(conString).use {
-                if(it.list().contains(name)) it.drop(name)
-                assertEquals("should not have test db after drop", false, it.list().contains(name))
-                it.create(name)
-                assertEquals("should have test db after create", true, it.list().contains(name))
-            }
-        } catch (ex: Exception) {
-            assertNull(ex)
+        val conString = "jdbc:postgresql://localhost:5432/postgres?user=postgres&password=postgres"
+        DbHelper(conString).use {
+            if(it.list().contains(name)) it.drop(name)
+            assertEquals("should not have test db after drop", false, it.list().contains(name))
+            it.create(name)
+            assertEquals("should have test db after create", true, it.list().contains(name))
+            // throw IllegalStateException("WTF")
         }
     }
 }
