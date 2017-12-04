ff593d7afa8a15c55e0587325add1436b764f114
Brent <bgardner@squarelabs.net>
2017-12-04 03:04:01 +0100

    Log exceptions?

--- src/test/kotlin/net/squarelabs/pgrepl/SlotHelperTest.kt
+++ src/test/kotlin/net/squarelabs/pgrepl/SlotHelperTest.kt
@@ -9,17 +9,12 @@ class SlotHelperTest {
 
     @Test
     fun shouldCrud() {
-        try {
-            val conString = "jdbc:postgresql://localhost:5432/$dbName?user=postgres&password=postgres"
-            SlotHelper(conString).use {
-                if(it.list().contains(dbName)) it.drop(dbName)
-                Assert.assertEquals("should not have test db after drop", false, it.list().contains(dbName))
-                it.create(dbName, "test_decoding")
-                Assert.assertEquals("should not test db after create", true, it.list().contains(dbName))
-            }
-        } catch (ex: Exception) {
-            ex.printStackTrace() // Get woke, travis
-            Assert.assertNull(ex)
+        val conString = "jdbc:postgresql://localhost:5432/$dbName?user=postgres&password=postgres"
+        SlotHelper(conString).use {
+            if(it.list().contains(dbName)) it.drop(dbName)
+            Assert.assertEquals("should not have test db after drop", false, it.list().contains(dbName))
+            it.create(dbName, "test_decoding")
+            Assert.assertEquals("should not test db after create", true, it.list().contains(dbName))
         }
     }
 
