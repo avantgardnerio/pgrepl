e31326e680947eb72473e292205c69ba142a3c68
Brent <bgardner@squarelabs.net>
2017-12-06 21:28:55 +0100

    Echo socket!

--- src/main/kotlin/net/squarelabs/pgrepl/ReplicationSocket.java
+++ src/main/kotlin/net/squarelabs/pgrepl/ReplicationSocket.java
@@ -1,11 +1,43 @@
 package net.squarelabs.pgrepl;
 
-import javax.websocket.EndpointConfig;
-import javax.websocket.Session;
+import org.eclipse.jetty.util.log.Log;
+import org.eclipse.jetty.util.log.Logger;
 
-public class ReplicationSocket extends javax.websocket.Endpoint {
+import javax.websocket.*;
+
+public class ReplicationSocket extends Endpoint implements MessageHandler.Whole<String> {
+    private static final Logger LOG = Log.getLogger(ReplicationSocket.class);
+    private Session session;
+    private RemoteEndpoint.Async remote;
+
     @Override
-    public void onOpen(Session session, EndpointConfig config) {
+    public void onClose(Session session, CloseReason close) {
+        super.onClose(session, close);
+        this.session = null;
+        this.remote = null;
+        LOG.info("WebSocket Close: {} - {}", close.getCloseCode(), close.getReasonPhrase());
+    }
 
+    public void onOpen(Session session, EndpointConfig config) {
+        this.session = session;
+        this.remote = this.session.getAsyncRemote();
+        LOG.info("WebSocket Connect: {}", session);
+        this.remote.sendText("You are now connected to " + this.getClass().getName());
+        // attach echo message handler
+        session.addMessageHandler(this);
+    }
+
+    @Override
+    public void onError(Session session, Throwable cause) {
+        super.onError(session, cause);
+        LOG.warn("WebSocket Error", cause);
+    }
+
+    @Override
+    public void onMessage(String message) {
+        LOG.info("Echoing back text message [{}]", message);
+        if (this.session != null && this.session.isOpen() && this.remote != null) {
+            this.remote.sendText(message);
+        }
     }
 }
