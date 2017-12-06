4729efd41195d8db333fa76acd1d4219ac3908d1
Brent <bgardner@squarelabs.net>
2017-12-06 21:21:36 +0100

    Setting up sockets

--- src/main/kotlin/net/squarelabs/pgrepl/Main.java
+++ src/main/kotlin/net/squarelabs/pgrepl/Main.java
@@ -1,11 +1,35 @@
 package net.squarelabs.pgrepl;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.websocket.jsr356.server.ServerContainer;
+import org.eclipse.jetty.websocket.jsr356.server.deploy.WebSocketServerContainerInitializer;
+
+import javax.websocket.server.ServerEndpointConfig;
+import java.net.URL;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
         server.setHandler(new HelloHandler());
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        ServerContainer container = WebSocketServerContainerInitializer.configureContext(context);
+        ServerEndpointConfig echoConfig = ServerEndpointConfig.Builder.create(ReplicationSocket.class, "/echo").build();
+        container.addEndpoint(echoConfig);
+        URL urlStatics = Thread.currentThread().getContextClassLoader().getResource("index.html");
+        Objects.requireNonNull(urlStatics, "Unable to find index.html in classpath");
+        String urlBase = urlStatics.toExternalForm().replaceFirst("/[^/]*$", "/");
+        ServletHolder defHolder = new ServletHolder("default", new DefaultServlet());
+        defHolder.setInitParameter("resourceBase", urlBase);
+        defHolder.setInitParameter("dirAllowed", "true");
+        context.addServlet(defHolder, "/");
+
         server.start();
         server.join();
     }
---
+++ src/main/kotlin/net/squarelabs/pgrepl/ReplicationSocket.java
@@ -0,0 +1,11 @@
+package net.squarelabs.pgrepl;
+
+import javax.websocket.EndpointConfig;
+import javax.websocket.Session;
+
+public class ReplicationSocket extends javax.websocket.Endpoint {
+    @Override
+    public void onOpen(Session session, EndpointConfig config) {
+
+    }
+}
