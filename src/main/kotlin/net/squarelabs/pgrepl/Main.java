b9597db6d26eb0677bd4f1a2f96284ac31d3e3a4
Brent <bgardner@squarelabs.net>
2017-12-06 20:59:07 +0100

    Jetty hello world

--- src/main/kotlin/net/squarelabs/pgrepl/Main.java
+++ src/main/kotlin/net/squarelabs/pgrepl/Main.java
@@ -5,6 +5,7 @@ import org.eclipse.jetty.server.Server;
 public class Main {
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
+        server.setHandler(new HelloHandler());
         server.start();
         server.join();
     }
