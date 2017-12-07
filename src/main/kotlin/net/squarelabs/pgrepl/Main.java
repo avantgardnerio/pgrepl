5ed1b762bbc5ab7257720e06a2d8a70bb3dd79be
Brent <bgardner@squarelabs.net>
2017-12-07 06:18:20 +0100

    Flyway

--- src/main/kotlin/net/squarelabs/pgrepl/Main.java
+++ src/main/kotlin/net/squarelabs/pgrepl/Main.java
@@ -1,11 +1,13 @@
 package net.squarelabs.pgrepl;
 
+import com.github.javafaker.Faker;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.websocket.jsr356.server.ServerContainer;
 import org.eclipse.jetty.websocket.jsr356.server.deploy.WebSocketServerContainerInitializer;
+import org.flywaydb.core.Flyway;
 
 import javax.websocket.server.ServerEndpointConfig;
 import java.net.URL;
@@ -13,16 +15,24 @@ import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Server server = new Server(8080);
-        server.setHandler(new HelloHandler());
 
+        // Database
+        DbHelper db = new DbHelper("jdbc:postgresql://localhost:5432/postgres?user=postgres&password=postgres");
+        db.drop("pgrepl_test");
+        db.create("pgrepl_test");
+        Flyway flyway = new Flyway();
+        flyway.setDataSource("jdbc:postgresql://localhost:5432/pgrepl_test", "postgres", "postgres");
+        flyway.migrate();
+
+        // Jetty
+        Server server = new Server(8080);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
         server.setHandler(context);
         ServerContainer container = WebSocketServerContainerInitializer.configureContext(context);
         ServerEndpointConfig echoConfig = ServerEndpointConfig.Builder.create(ReplicationSocket.class, "/echo").build();
         container.addEndpoint(echoConfig);
-        URL urlStatics = Thread.currentThread().getContextClassLoader().getResource("index.html");
+        URL urlStatics = Thread.currentThread().getContextClassLoader().getResource("static/index.html");
         Objects.requireNonNull(urlStatics, "Unable to find index.html in classpath");
         String urlBase = urlStatics.toExternalForm().replaceFirst("/[^/]*$", "/");
         ServletHolder defHolder = new ServletHolder("default", new DefaultServlet());
@@ -30,6 +40,7 @@ public class Main {
         defHolder.setInitParameter("dirAllowed", "true");
         context.addServlet(defHolder, "/");
 
+        // Start Jetty
         server.start();
         server.join();
     }
