85186657d100723bf2585eb85a940cc128a053f4
Brent <bgardner@squarelabs.net>
2017-12-18 14:36:57 +0100

    Able to play interleaved transactions

--- src/test/kotlin/net/squarelabs/pgrepl/db/ReplicatorTest.kt
+++ src/test/kotlin/net/squarelabs/pgrepl/db/ReplicatorTest.kt
@@ -50,20 +50,39 @@ class ReplicatorTest {
         conSvc.getConnection(conString).use {
             snap = snapSvc.takeSnapshot(it)
         }
-        var actual = ""
-        val spy = { lsn: Long, json: String -> actual = json }
+        val actual = mutableListOf<String>()
+        val spy = { lsn: Long, json: String ->
+            actual.add(json)
+            Unit
+        }
         Replicator(dbName, clientId, snap!!.lsn, cfgSvc, conSvc).use {
             it.addListener(spy)
-            conSvc.getConnection(conString).use {
-                val sql = "INSERT INTO person (id, name, curTxnId) VALUES (1, 'Brent', 'd55cad5c-03da-405f-af3a-13788092b33c');"
-                it.prepareStatement(sql).use {
-                    it.executeUpdate()
+            conSvc.getConnection(conString).use { conA ->
+                conSvc.getConnection(conString).use { conB ->
+                    conA.autoCommit = false
+                    conB.autoCommit = false
+                    conA.prepareStatement("INSERT INTO person (id, name, curTxnId) VALUES (1, 'Brent', 'd55cad5c-03da-405f-af3a-13788092b33c');").use {
+                        it.executeUpdate()
+                    }
+                    conB.prepareStatement("INSERT INTO person (id, name, curTxnId) VALUES (2, 'Rachel', '794d1570-ce12-4371-9304-0d50cce518ca');").use {
+                        it.executeUpdate()
+                    }
+                    conA.prepareStatement("INSERT INTO person (id, name, curTxnId) VALUES (3, 'Emma', 'ee52c2be-8690-4bbb-9cac-a4aa5e7ca81e');").use {
+                        it.executeUpdate()
+                    }
+                    conB.prepareStatement("INSERT INTO person (id, name, curTxnId) VALUES (4, 'Annie', '36bc5f56-2c7d-4147-af72-f03bd1443e9e');").use {
+                        it.executeUpdate()
+                    }
+                    conA.commit()
+                    TimeUnit.MILLISECONDS.sleep(100) // TODO: No hard-coded waits
+                    conB.commit()
+                    TimeUnit.MILLISECONDS.sleep(100) // TODO: No hard-coded waits
                 }
             }
-            TimeUnit.MILLISECONDS.sleep(100) // TODO: No hard-coded waits
+            TimeUnit.MILLISECONDS.sleep(1000) // TODO: No hard-coded waits
         }
 
-        val actualObj: Transaction = Gson().fromJson(actual, Transaction::class.java)
+        val actualObj: Transaction = Gson().fromJson(actual.get(0), Transaction::class.java)
                 .copy(clientTxnId = "d55cad5c-03da-405f-af3a-13788092b33c")
         val expectedObj: Transaction = Gson().fromJson(expected, Transaction::class.java)
                 .copy(xid = actualObj.xid, clientTxnId = "d55cad5c-03da-405f-af3a-13788092b33c")
