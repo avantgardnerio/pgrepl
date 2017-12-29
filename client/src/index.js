fa6863f5289b0ebb30b2b5fbf54fc6fb6adde398
Brent <bgardner@squarelabs.net>
2017-12-29 12:50:11 -0500

    Redux is confusing :/

--- client/src/index.js
+++ client/src/index.js
@@ -5,15 +5,24 @@ import {Provider} from 'react-redux';
 
 import './index.css';
 import App from './containers/App';
-import { logSender } from './middleware/logSender';
+import { createLogSender } from './middleware/logSender';
 import reducer from './reducers';
 import SocketService from './SocketService';
+import Database from "./Database";
 
 const elementIds = ['leftRoot','rightRoot'];
 elementIds.forEach((elementId) => {
+
+    const init = async () => {
+        const db = new Database(elementId);
+        const version = await db.connect();
+        console.log('Connected to DB version', version);
+    };
+    init();
+
     const ws = new SocketService();
     ws.onConnect = () => ws.write({type: 'HELLO', payload: ws.id});
-    const ls = logSender(ws);
+    const ls = createLogSender(ws);
     const store = createStore(reducer, applyMiddleware(ls));
     ws.onMsg = (msg) => store.dispatch(msg);
     ws.connect();
--- client/src/middleware/logSender.js
+++ client/src/middleware/logSender.js
@@ -1,15 +1,24 @@
-export const logSender = (socket) => () => (next) => (action) => {
-    switch (action.type) {
-        case 'COMMIT':
-            try {
-                socket.write(action);
-                return next(action);
-            } catch(ex) {
-                // TODO: retry on reconnect
-                console.error("Error sending transaction to server!", ex);
-            }
-            break;
-        default:
-            return next(action);
-    }
+export const createLogSender = (socket) => {
+    const logSender = () => {
+        const wrapDispatch = (next) => {
+            const dispatch = (action) => {
+                switch (action.type) {
+                    case 'COMMIT':
+                        try {
+                            socket.write(action);
+                            return next(action);
+                        } catch (ex) {
+                            // TODO: retry on reconnect
+                            console.error("Error sending transaction to server!", ex);
+                        }
+                        break;
+                    default:
+                        return next(action);
+                }
+            };
+            return dispatch;
+        };
+        return wrapDispatch;
+    };
+    return logSender;
 };
