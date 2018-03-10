acf7386afa2c7a6887f763e100e5042225039643
Brent <bgardner@squarelabs.net>
2018-03-10 10:58:14 -0700

    Starting over in node

--- client/src/util/math.js
+++
@@ -1,8 +0,0 @@
-export const add = (a, b) => [a[0] + b[0], a[1] + b[1]];
-export const subtract = (a, b) => [a[0] - b[0], a[1] - b[1]];
-export const equals = (a, b) => {
-    if (!a && !b) return true;
-    if (!a || !b) return false;
-    return JSON.stringify(a) === JSON.stringify(b);
-};
-export const unique = (ar) => [...new Set(ar)];
