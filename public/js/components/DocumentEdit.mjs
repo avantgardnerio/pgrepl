6b3a1b1e0fc3da32d10974b1b93ebdf7de34800f
Brent Gardner <bgardner@squarelabs.net>
2018-03-25 04:03:26 -0600

    Working aspect ratio

--- public/js/components/DocumentEdit.mjs
+++ public/js/components/DocumentEdit.mjs
@@ -10,10 +10,8 @@ export default class DocumentEdit {
         const html = `
         <svg style="width: 100%; height: 100%" viewbox="0 0 1 1" 
                 preserveAspectRatio="xMidyMid meet">
-            <line x1="0.1" y1="0.1" 
-                x2="0.9" y2="0.9" 
-                stroke-width="2" stroke="black" 
-                vector-effect="non-scaling-stroke"/>
+            <rect x="0" y="0" width="1" height="1" stroke-width="2" stroke="black" vector-effect="non-scaling-stroke" 
+                fill-opacity="0"/>
         </svg>`;
         this.el = new DOMParser().parseFromString(html, `text/html`).body.firstChild;
 
@@ -24,9 +22,24 @@ export default class DocumentEdit {
         this.onChange();
     }
 
+    screenToView(pos) {
+        if(this.el.clientHeight > this.el.clientWidth) {
+            const offset = (this.el.clientHeight - this.el.clientWidth) / 2;
+            const x = pos[0] / this.el.clientWidth;
+            const y = (pos[1] - offset) / this.el.clientWidth;
+            return [x, y];
+        } else {
+            const offset = (this.el.clientWidth - this.el.clientHeight) / 2;
+            const y = pos[1] / this.el.clientHeight;
+            const x = (pos[0] - offset) / this.el.clientHeight;
+            return [x, y];
+        }
+    }
+
     onMouseDown(e) {
-        this.src = [e.offsetX / this.el.clientWidth, e.offsetY / this.el.clientHeight];
-        this.dst = [e.offsetX / this.el.clientWidth, e.offsetY / this.el.clientHeight];
+        const pos = [e.offsetX, e.offsetY];
+        this.src = this.screenToView(pos);
+        this.dst = this.screenToView(pos);
         this.currentEl = document.createElementNS("http://www.w3.org/2000/svg", 'line');
         this.currentEl.setAttribute("x1", this.src[0]);
         this.currentEl.setAttribute("y1", this.src[1]);
@@ -40,7 +53,8 @@ export default class DocumentEdit {
 
     onMouseMove(e) {
         if (!this.currentEl) return;
-        this.dst = [e.offsetX / this.el.clientWidth, e.offsetY / this.el.clientHeight];
+        const pos = [e.offsetX, e.offsetY];
+        this.dst = this.screenToView(pos);
         this.currentEl.setAttribute(`x2`, this.dst[0]);
         this.currentEl.setAttribute(`y2`, this.dst[1]);
     }
